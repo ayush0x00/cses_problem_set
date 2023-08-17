6f069de75529a7c15e037f2b7e3379a92dc5b39c
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2023-08-18 00:27:59 +0530

    Interesting Dijkstra questions

--- graph/flight_discount.cpp
+++ graph/flight_discount.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve(){
+const long long INF=1e17;
 
+auto comp = [](pair<int,int>&a, pair<int,int>&b){
+    return a.second > b.second;
+};
+
+vector<long long> dijkstra(int src,const vector<vector<pair<int,int>>>&graph){
+    vector<long long>dist(graph.size()+1,INF);
+    dist[src]=0;
+    vector<bool>vis(graph.size()+1);
+    priority_queue<pair<long long,long long>,vector<pair<long long,long long>>,greater<pair<long long,long long>>>pq;
+    pq.push({0,src});
+
+    while(!pq.empty()){
+        int from = pq.top().second;
+        pq.pop();
+        if(vis[from] || dist[from]==INF) continue;
+        vis[from]=1;
+        for(auto nbr:graph[from]){
+            int to=nbr.first;
+            long long cost = nbr.second;
+            if(dist[to]>dist[from]+cost){
+                dist[to]=dist[from]+cost;
+                dist[to]=min(dist[to],INF);
+                pq.push({dist[to],to});
+            }
+        }
+    }
+
+    return dist;
 }
 
 int main(){
@@ -18,45 +46,32 @@ int main(){
     cin>>n>>m;
 
     vector<vector<pair<int,int>>>graph(n+1);
+    vector<vector<pair<int,int>>>rev_graph(n+1);
+    vector<vector<int>>edges;
 
     for(int i=1;i<=m;i++){
-            int from,to,cost;
+        int from,to,cost;
         cin>>from>>to>>cost;
         graph[from].push_back({to,cost});
+        rev_graph[to].push_back({from,cost});
+        edges.push_back({from,to,cost});
     }
 
-    vector<int>parent(n+1);
-    vector<int>back_dist(n+1);
-    vector<bool>vis(n+1);
-    vector<int>dist(n+1,INT_MAX);
-    dist[1]=0;
-    priority_queue<pair<int,int>>q;
-    q.push({1,0});
-    while(!q.empty()){
-        int from = q.top().first;
-        q.pop();
-        if(dist[from]==INT_MAX || vis[from]) continue;
-        vis[from]=true;
-        for(auto nbr:graph[from]){
-            int to = nbr.first;
-            int cost = nbr.second;
-            if(dist[from]+cost < dist[to]){
-                dist[to] = dist[from]+cost;
-                q.push({to,dist[to]});
-                parent[to]=from;
-                back_dist[to]=cost;
-            }
-        }
-    }
-    int max_cost  = INT_MIN;
-    for(int cur=n;cur!=1;cur=parent[cur]){
-        // cout<<cur<<"->";
-        max_cost=max(max_cost,back_dist[cur]);
-    }
-    // cout<<max_cost<<"\n";
-    int reduction = max_cost/2;
+    vector<long long>dist = dijkstra(1,graph);
+    vector<long long>rev_dist = dijkstra(n,rev_graph);
+
     // for(auto i:dist) cout<<i<<" ";
     // cout<<"\n";
-    cout<<dist[n]-max_cost+reduction<<"\n";
+    // for(auto i:rev_dist) cout<<i<<" ";
+    // cout<<"\n";
+
+    long long ans=INF;
+    for(auto e:edges){
+        int from = e[0],to=e[1],cost=e[2];
+        ans=min(ans, dist[from]+cost/2+rev_dist[to]);
+    }
+
+    cout<<ans<<"\n";
+    
     return 0;
 }
---
+++ graph/flight_routes.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const long long INF = 1e17;
+
+vector<vector<long long>> dijkstra(int src, const vector<vector<pair<int, int>>> &graph, int k)
+{
+    priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
+    pq.push({0, src});
+
+
+    vector<vector<long long>> dist(graph.size()+1,vector<long long>(k,INF));
+
+    dist[src][0]=0;
+
+    while (!pq.empty())
+    {
+        int from = pq.top().second;
+        long long d = pq.top().first;
+        pq.pop();
+
+        // if(vis[from]) continue;
+        if(dist[from][k-1] < d) continue;
+        for(auto nbr:graph[from]){
+            int to = nbr.first;
+            int cost = nbr.second;
+            if(dist[to][k-1]>d+cost){
+                dist[to][k-1]=cost+d;
+                pq.push({dist[to][k-1],to});
+                sort(dist[to].begin(),dist[to].end());
+            }
+            
+        }
+    }
+
+    // for(auto i:dist) cout<<i<<" ";
+
+    return dist;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+    freopen("test_input.txt", "r", stdin);
+#endif
+
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<vector<pair<int, int>>> graph(n + 1);
+    for (int i = 1; i <= m; i++)
+    {
+        int from, to, cost;
+        cin>>from>>to>>cost;
+        graph[from].push_back({to, cost});
+    }
+
+    vector<vector<long long>>dist = dijkstra(1,graph,k);
+    for(int i=0;i<k;i++){
+        cout<<dist[n][i]<<" ";
+    }
+}
